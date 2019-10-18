398501a45a3119ed35dbf68e3a40c4cadb42439e
Ryosuke Uchitate <ruchitate@coiney.com>
2019-10-18 15:58:00 +0900

    レスポンス変更

--- src/main/java/com/b1a9idps/springcloudcontractsample/producer/controller/BrandController.java
+++ src/main/java/com/b1a9idps/springcloudcontractsample/producer/controller/BrandController.java
@@ -1,5 +1,7 @@
 package com.b1a9idps.springcloudcontractsample.producer.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,10 @@ import com.b1a9idps.springcloudcontractsample.producer.dto.Brand;
 public class BrandController {
 
     @GetMapping
-    public Brand list() {
+    public List<Brand> list() {
         Brand stof = new Brand();
         stof.setName("STOF");
         stof.setDesigner("Tanita");
-        return stof;
+        return List.of(stof);
     }
 }
