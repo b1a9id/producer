46711cdd36c5b48596a7b5524c74aee075e80648
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2019-11-15 15:38:09 +0900

    Spring REST Docsと連携

--- src/test/java/com/b1a9idps/springcloudcontractsample/producer/TestBase.java
+++ src/test/java/com/b1a9idps/springcloudcontractsample/producer/TestBase.java
@@ -2,30 +2,34 @@ package com.b1a9idps.springcloudcontractsample.producer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
-
-import com.b1a9idps.springcloudcontractsample.producer.controller.BrandController;
+import org.springframework.web.context.WebApplicationContext;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
-@ExtendWith(SpringExtension.class)
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @DirtiesContext
 @AutoConfigureMessageVerifier
 public abstract class TestBase {
 
-    @Autowired
-    private BrandController brandController;
-
     @BeforeEach
-    public void setup() {
-        StandaloneMockMvcBuilder standaloneMockMvcBuilder = MockMvcBuilders.standaloneSetup(brandController);
-        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+    public void setup(
+            WebApplicationContext context,
+            RestDocumentationContextProvider restDocumentation) {
+        RestAssuredMockMvc.mockMvc(
+                MockMvcBuilders.webAppContextSetup(context)
+                        .apply(documentationConfiguration(restDocumentation))
+                        .alwaysDo(document("index"))
+                        .build());
     }
 }
